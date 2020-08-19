5dd0c1c9261240fba4dc722ea09068a653564637
Ilya Dorogan <enotshannel@gmail.com>
2020-08-19 14:33:19 +0300

    lesoon_51_end

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 "use strict";
 
 /* LESSON 51 JSON формат передачи данных, глубокое клонирование объектов*/
+
+/*
+const persone = {
+	name: 'Ilya',
+	tel: '+38012345678',
+	parents: {
+		father: 'Yuri',
+		mother: 'Olga'
+	}
+};
+
+// console.log(JSON.stringify(persone));  
+// console.log(JSON.parse(JSON.stringify(persone)));
+
+const clone = JSON.parse(JSON.stringify(persone)); //глубокая копия  
+
+clone.parents.mother = 'None';
+
+console.log(persone);
+console.log(clone);
+*/
